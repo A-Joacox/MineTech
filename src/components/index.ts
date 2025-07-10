fc113774a40993600bfa7342bf4eb0c5bad7a35e
joaquin_ <joacmercado@gmail.com>
2025-07-10 00:19:17 -0500

    updated

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Navbar } from './Navbar';
-export { default as ProtectedRoute } from './ProtectedRoute'; 
